99a6f9d803457e049d02e0b77353e1325d24a73b
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-07-30 16:01:03 +0500

    use hex ctrl for some unk1 values (which are actually flags), use grid on unk1 to align items

--- yaean/helpers.py
+++ yaean/helpers.py
@@ -49,7 +49,7 @@ def build_bone_tree(bone_list_ctrl, esk):
 
 # MY MODIF
 def build_unk1_list(unk_list, esk):
-    from pyxenoverse.esk import I_IDX_TO_NAME, I_BYTE_ORDER, THESE_POINT_TO_BONES
+    from pyxenoverse.esk import I_IDX_TO_NAME, I_BYTE_ORDER, THESE_POINT_TO_BONES, FLAG_VALS
     for idx in range(len(unk_list)):
         if I_IDX_TO_NAME[idx] in THESE_POINT_TO_BONES:
             unk_list[idx].Clear()
@@ -62,6 +62,8 @@ def build_unk1_list(unk_list, esk):
             unk_list[idx].Enable()
         elif I_BYTE_ORDER[idx].lower() == 'f':
             unk_list[idx].SetValue(esk.unk1_list[idx])
+        elif I_IDX_TO_NAME[idx] in FLAG_VALS:
+            unk_list[idx].SetValue(int(esk.unk1_list[idx]))
         else:
             unk_list[idx].SetValue(str(esk.unk1_list[idx]))
 
@@ -182,5 +184,5 @@ def quaternion_to_euler(q):
 
 # MY MODIF
 def convert_to_px(dip, width=True):
-        dc = wx.ScreenDC()
-        return int(dip * (dc.GetPPI().width if width else dc.GetPPI().height) / 96.0)
+    dc = wx.ScreenDC()
+    return int(dip * (dc.GetPPI().width if width else dc.GetPPI().height) / 96.0)
